75774aa2fc843d8768184c6e3e69ee23f428763f
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2024-06-16 20:30:07 -0300

    add

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}`)
+if (vel > 60 ) {
+    console.log(`Você ultrapassou a velocidade permitida da via você será multado`)
+}
+console.log('Dirija sempre com sinto de segurança')
+
+
+
