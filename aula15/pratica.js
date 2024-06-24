9e7a9cbbd6bc6ebef50f727edfaabc1daf7427f4
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2024-06-24 19:08:14 -0300

    add

---
+++ aula15/pratica.js
@@ -0,0 +1,15 @@
+var num = [50,0,20,1,9,15,10,50,0,20,1,9,10]
+num.push(22)
+num.sort()
+
+/*for (var c = 0; c < num.length;c++) {
+    console.log(` A posição ${c} tem o valor ${num[c]}`)
+}
+  */  
+
+for (var c in num) {
+    console.log(num[c])
+}
+
+console.log(`O número 15 está na posição ${num.indexOf(15)}`)
+console.log(`O número 18 está na posição ${num.indexOf(18)}`)
