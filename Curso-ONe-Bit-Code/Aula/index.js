e1a62233bea75dae24efa59e58056d8c04a8c39b
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2024-07-17 20:59:08 -0300

    modfiq

---
+++ Curso-ONe-Bit-Code/Aula/index.js
@@ -0,0 +1,25 @@
+
+/*
+let nome = window.prompt('Digite seu primeiro nome:')
+
+let subnome = prompt('Digite seu sobre nome:')
+
+let estuda = prompt('Qual o seu campo de estudo?')
+
+let ano = prompt('Qual o seu ano de nascimento?')
+
+alert(`Seu primeiro nome é ${nome} 
+    o seu sobre nome é ${subnome}, 
+    seu campo de estudo é ${estuda} 
+    e nasceu no ano ${Number(2024) - Number(ano)}`) */
+
+let n1 = prompt('Digite um número:')
+
+let n2 = prompt('Digite outro número:')
+
+alert(`A soma entre os dois números é ${Number(n1) + Number(n2)}. 
+A subtração entre os dois é ${Number(n1) - Number(n2)}. 
+A divisão entre os dois é ${Number(n1) / Number(n2)}.
+ A multiplicação entre os dois é ${Number(n1) * Number(n2)}.
+ E o resto da divisão entre eles é ${Number(n1) % Number(n2)}`) 
+
