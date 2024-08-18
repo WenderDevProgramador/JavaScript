d9342c6af77051872a2cdf54612ef81378651cc1
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2024-08-18 18:01:18 -0300

    add

--- Curso-ONe-Bit-Code/Aula/teste/index.js
+++ Curso-ONe-Bit-Code/Aula/teste/index.js
@@ -1,89 +1,90 @@
 
-function createLabel(text, htmlFor) {
+function criaRLabel(text,htmlFor){
     const label = document.createElement('label')
     label.innerText = text
     label.htmlFor = htmlFor
     return label
 }
 
-function createInput(id, value, name, type = "text", placeholder = '') {
+function criarInput(id,value,name,type='text',placeholder='') {
     const input = document.createElement('input')
     input.id = id
-    input.name = name
     input.value = value
+    input.name = name
     input.type = type
     input.placeholder = placeholder
     return input
 }
 
-const addTechBtn = document.getElementById('addTechBtn')
+const adicionar = document.getElementById('addTechBtn')
 const form = document.getElementById('devform')
-const developers = []
-let inputRows = 0
+const desenvolvedores = []
+let contInput = 0
 
-addTechBtn.addEventListener('click', (ev) => {
-    const stackinputs = document.getElementById('stackinputs')
+adicionar.addEventListener('click',(ev)=>{
+    const ul = document.getElementById('stackinputs')
+    const li = document.createElement('li')
+    const nomeId = contInput
+    contInput++
+    li.id = 'inputRow' + nomeId
+    li.className = 'inputRow'
+    const techLabel = criaRLabel('Nome: ','techname-' + nomeId)
+    const techInput = criarInput('techname-' + nomeId,null,'techname')
 
-    const newRow = document.createElement('li')
-    const rowIndex = inputRows
-    inputRows++
-    newRow.id = 'inputRow-' + rowIndex
-    newRow.className = 'inputRow'
+    const experiencia = criaRLabel('Experiência: ')
 
-    const techNameLabel = createLabel('Nome: ', 'techname-' + rowIndex)
-    const techinput = createInput('techName-' + rowIndex, null, 'techName')
+    const id1 = 'ExpRadio' + nomeId + '.1'
+    const expInput1 = criarInput(id1,'0-2 anos','techExp-' + nomeId,'radio')
+    const explabel1 = criaRLabel('0-2 anos',id1)
 
-    const experiencia = createLabel('Experiência: ')
+    const id2 = 'ExpRadio' + nomeId + '.2'
+    const expInput2 = criarInput(id2,'3-4 anos','techExp-' + nomeId,'radio')
+    const explabel2 = criaRLabel('3-4 anos',id2)
 
-    const id1 = 'expRadio' + rowIndex + '.1'
-    const expRadio1 = createInput(id1, '0-2 anos', 'techExp-' + rowIndex, 'radio')
-    const expLabel1 = createLabel('0-2 anos', id1)
+    const id3 = 'ExpRadio' + nomeId + '.3'
+    const expInput3 = criarInput(id3,'5 + anos','techExp-' + nomeId,'radio')
+    const explabel3 = criaRLabel('5 + anos',id3)
 
-    const id2 = 'expRadio' + rowIndex + '.2'
-    const expRadio2 = createInput(id2, '0-2 anos', 'techExp-' + rowIndex, 'radio')
-    const expLabel2 = createLabel('0-2 anos', id2)
+    const remover = document.createElement('button')
+    remover.type = 'button'
+    remover.innerText = 'Remover'
 
-    const id3 = 'expRadio' + rowIndex + '.3'
-    const expRadio3 = createInput(id3, '0-2 anos', 'techExp-' + rowIndex, 'radio')
-    const expLabel3 = createLabel('0-2 anos', id3)
-
-    const removeRowBnt = document.createElement('button')
-    removeRowBnt.type = 'button'
-    removeRowBnt.innerText = 'Remover'
-    removeRowBnt.addEventListener('click', () => {
-        stackinputs.removeChild(newRow)
+    remover.addEventListener('click',(ev)=> {
+        ul.removeChild(li)
     })
 
 
 
-
-    newRow.append(techNameLabel, techinput, experiencia, expRadio1, expLabel1, expRadio2, expLabel2, expRadio3, expLabel3, removeRowBnt)
-
-    stackinputs.append(newRow)
+    li.append(techLabel,techInput,experiencia,expInput1,explabel1,expInput2,explabel2,expInput3,explabel3,remover)
+    ul.append(li)
 })
 
-form.addEventListener('submit', (ev) => {
+form.addEventListener('submit',(ev)=> {
     ev.preventDefault()
 
-    const fullNameInput = document.getElementById('fullname')
-    const inputsRow = document.querySelectorAll('.inputRow')
+    const nomeInput = document.getElementById('fullname')
+    const liTodas = document.querySelectorAll('.inputRow')
 
-    let technnologies = []
+    let tecnologias = []
 
-    inputRows.forEach((row) => {
-        const techName = document.querySelector('#' + row.id + 'input[name="techName"]').value
-        const techExp = document.querySelector('#' + row.id + 'input[type="radio"]:checked').value
-        technnologies.push({ name: techName, exp: techExp })
+    liTodas.forEach((linha) => {
+        const novatech = document.querySelector('#'+ linha.id + ' input[name="techname"]').value
+        const tempoExp = document.querySelector('#'+ linha.id + ' input[type="radio"]:checked').value
+
+        tecnologias.push({name:novatech,exp:tempoExp})
     })
 
-    const newDev = {fullname: fullNameInput.value, technnologies}
-    developers.push(newDev)
-    alert('Dev cadastrado com sucesso!')
+    const newDev = {Nomecompleto: nomeInput.value,tecnologias}
 
-    fullNameInput.value = ''
-    inputRows.forEach((row)=> {
-        row.remove()
+    desenvolvedores.push(newDev)
+    alert('Desenvolvedor cadastrado com sucesso!')
+
+
+    nomeInput.value = ''
+    liTodas.forEach((linha) => {
+        linha.remove()
     })
 
-    console.log(developers)
+    console.log(desenvolvedores)
+
 })
