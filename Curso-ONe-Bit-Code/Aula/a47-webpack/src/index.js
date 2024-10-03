66be87644e35a95037014bf6f7ca80fec385687e
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2024-10-03 18:56:48 -0300

    ad

--- Curso-ONe-Bit-Code/Aula/a47-webpack/src/index.js
+++ Curso-ONe-Bit-Code/Aula/a47-webpack/src/index.js
@@ -1,3 +1,4 @@
 import dayjs from 'dayjs'
+import './index.css'
 
 alert(`HOje é: ${dayjs().format("DD/MM/YYYY")}`)
--- Curso-ONe-Bit-Code/Aula/a47-webpack/webpack.config.js
+++ Curso-ONe-Bit-Code/Aula/a47-webpack/webpack.config.js
@@ -1,11 +1,20 @@
-const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: {
         index: './src/index.js',
         
     },
-    mode: 'production',
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.css$/, // Tipo de arquivo, no caso aqui o ponto só vai funcionar $ é para indicar que procuramos no final do arquivo.
+            use: [MiniCssExtractPlugin.loader,'css-loader'] // Tipo de loader que iremos usar
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
     
     }
 
