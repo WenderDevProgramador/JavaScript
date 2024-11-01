d89d592bd6b0c4773a9aca8e88816e9e700c71f1
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2024-11-01 12:27:39 -0300

    add

--- Curso-ONe-Bit-Code/Aula/a60-projeto-poo/App.js
+++ Curso-ONe-Bit-Code/Aula/a60-projeto-poo/App.js
@@ -1,11 +1,14 @@
 const Database = require("./Database.js")
 const Author = require("./entities/Author.js")
 const User = require("./entities/User.js")
+const Book = require("./entities/Book.js")
+const Poster = require("./entities/Poster.js")
+const Order = require("./entities/Order.js")
 
 module.exports = class App {
     static #database = new Database()
 
-    createuser(name,email,password) {
+    createUser(name,email,password) {
         const user = new User(name, email, password)
         App.#database.saveUser(user)
     }
@@ -22,4 +25,43 @@ module.exports = class App {
     getAuthor() {
         return App.#database.find('authors')
     }
+
+    createBook(title, synopsis, genre, pages, author, description, price, inStock) {
+        const book = new Book(title,synopsis,genre,pages,author,description,price,inStock)
+        App.#database.saveBook(book)
+    }
+
+    addBook(bookName, quantity) {
+        App.#database.addBooksToStock(bookName, quantity)
+    }
+
+    createPoster( name, description, height, width, price, inStock) {
+        const poster = new Poster(name,height, width,description,price,inStock)
+        App.#database.savePoster(poster)
+    }
+
+    addPoster(posterName, quantity) {
+        App.#database.addPostersToStock(posterName, quantity)
+    }
+
+    createOrder(items, user) {
+        const order = new Order(items, user)
+        App.#database.saveOrder(order)
+        order.data.items.forEach(({product, quantity}) => {
+            if (product instanceof Book) {
+                App.#database.removeBooksFromStock(product.name, quantity)
+            } else if (product instanceof Poster) {
+                App.#database.removePostersFromStock(product.name, quantity)
+            }
+        })
+    }
+
+    getorders() {
+        return App.#database.find('orders')
+    }
+
+    showDatabase() {
+        App.#database.showStorage()
+    }
+
 } 
