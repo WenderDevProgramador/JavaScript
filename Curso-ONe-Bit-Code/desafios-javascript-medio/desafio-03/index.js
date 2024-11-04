770d0cedf200971782439d56cf170d17a2f80596
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2024-11-04 19:12:55 -0300

    add

---
+++ Curso-ONe-Bit-Code/desafios-javascript-medio/desafio-03/index.js
@@ -0,0 +1,11 @@
+/*
+Escreva uma função que recebe uma string e um número e retorne o resultado da aplicação da Cifra de Cesar para descriptografar o seu conteúdo, ou seja , que retroceda cada letra pelo número passado seguindo a ordem alfabética.
+
+- Entrada: ('Vguvg',2)
+Saída: 'Teste'
+
+- Entrada: ('BCDYZAbcdyza',27)
+Saída:'ABCXYZabcxyz'
+
+- Entrada: ('Qaiik',60)
+Saída: 'Isaac'*/ 
