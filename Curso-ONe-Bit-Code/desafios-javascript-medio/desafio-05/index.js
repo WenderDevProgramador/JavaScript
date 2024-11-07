cf6fb67d5ac71c848a6aaeaa7b3d017bd6b71461
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2024-11-07 20:01:01 -0300

    add

--- Curso-ONe-Bit-Code/desafios-javascript-medio/desafio-05/index.js
+++ Curso-ONe-Bit-Code/desafios-javascript-medio/desafio-05/index.js
@@ -16,4 +16,55 @@ class Equation {
         this.b = b
         this.c = c
     }
+
+    getRoots () {
+        if (this.a === 0 && this.b === 0 && this.c === 0) {
+            return 'Erro ! Nenhum parâmetro informado!'
+        }
+
+        if (this.a === 0 && this.b === 0) {
+            console.log('1. Parâmetros insuficientes.\nNenhuma raiz.')
+            return []
+        }
+
+        if (this.a === 0 ) {
+            console.log(`1. ${this.b} X + ${this.c} = 0`)
+            console.log(`2. ${this.b} X = ${this.c * -1} `)
+            console.log(`3. X = ${this.c * -1} / ${this.b} = 0`)
+            console.log(`4. x = ${(this.c * -1) / this.b}`)
+
+            return [(this.c * -1) / this.b]
+        }
+
+        const delta = this.b ** 2 - (4*this.a * this.c)
+        console.log(`1. Delta = ${this.b}² - 4 * ${this.a} * ${this.c}`)
+        console.log(`2. Delta = ${delta}`)
+
+        if (delta < 0){
+            console.log(`Delta < 0 Nenhuma raiz.`)
+
+            return []
+        }
+
+        console.log(`3. X' = (- (${this.b})) + raiz ${delta} / 2* ${this.a}`)
+        console.log(`4. X" = (- (${this.b})) + raiz ${delta} / 2* ${this.a}`)
+        console.log(`5. X' = ${this.b * -1} + raiz ${delta} / ${this.a * 2}` )
+        console.log(`6. X' = ${this.b * -1} - raiz ${delta} / ${this.a * 2}` )
+
+        const x1 = (this.b + Math.sqrt(delta) / (2 * this.a))
+        const x2 = (this.b - Math.sqrt(delta) / (2 * this.a))
+
+        if (Number.isInteger(Math.sqrt(delta))) {
+            console.log(`7. X' = ${this.b * -1} + ${Math.sqrt(delta)} / ${this.a * 2}`)
+            console.log(`8. X" = ${this.b * -1} - ${Math.sqrt(delta)} / ${this.a * 2}`)
+            console.log(`9. X' = ${x1} `)
+            console.log(`10. X" = ${x2} `)
+        }
+
+        return x1 === x2 ? [x1] : [x1,x2]
+    }
 }
+
+const equação = new Equation(3,4,5)
+
+console.log(equação.getRoots())
---
+++ Curso-ONe-Bit-Code/desafios-javascript-medio/desafio-06/index.js
@@ -0,0 +1,12 @@
+/*
+    Escreva uma função que recebe um número e retorna a sua persistência multiplicativa, que é a quantidade de vezes que é necessário multiplicar os seus algarismos para se chegar em um número de um únco algarismo.
+
+    Entrada : (539)
+    Saida : 3
+
+    Entrada : (999)
+    Saida : 4
+
+    Entrada : (7)
+    Saida : 0
+*/
