69da10d2f446ea82c310196860c39a51376cc203
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2024-11-10 12:16:29 -0300

    add

---
+++ Curso-ONe-Bit-Code/Aula/a63- exercico/index.js
@@ -0,0 +1,26 @@
+/*
+    Construa uma página HTML contendo um formulário de registro com os campos nome, email e senha onde , ao submeter o formulário, deverá ser feita uma validação para verificar se o email é um email válido e se a senha atende as regras mínimas de segurança.
+
+    Para que o email sejá válido ele deve possuir: 
+
+    @ texto antes do @ pelo menos 2 caracteres, números ou_
+
+    Texto após o @ com pelo menos 2 caracteres
+
+    Um ponto seguido de mais texto pelo menos 2 caracteres após o text após o @ 
+
+    ex: xx@xx.xx
+
+
+    Para que o senha sejá válido ele deve possuir:
+
+    1 minúscula
+    1 maiúscula
+    1 número 
+    1 Caractere especial
+    8 caracteres
+
+
+    Validações em funções separadas , apresentando mensagens apropriadas e evitando que o erro apareça no console do navegador.
+*/
+
