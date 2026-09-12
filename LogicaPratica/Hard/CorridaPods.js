e842f467c9dada9a3649fa4f8f7add8cb03632f0
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2026-09-12 17:20:13 -0300

    init

---
+++ LogicaPratica/Hard/CorridaPods.js
@@ -0,0 +1,15 @@
+// Escreva um programa que recebe uma lista de classificação de nomes e uma string no formato "Nome +n" (ou -n), onde n é a quantidade de posições para subir ou descer na classificação, e retorne essa mesma lista com a classificação atualizada. A função também deve ser capaz de receber "Nome ELIMINATE", nesse caso o participante deve ser jogado para o fim da lista  e deve ser acrescentado um "ELIMINATED" ao seu nome indicando que foi eliminado.
+
+// Os participantes eliminados não podem ter nenhum corredor não eliminado atras deles na lista.
+
+// Assuma que sempre receberar uma entrada válida no formato "Corredor AÇÃO".
+
+// Testes:  
+
+// Classificação inicial: 'Alfa', 'Beta', 'Gama' e 'Delta'.
+// Entrada: ('Beta + 1')
+// Classificação: 'Beta', 'Alfa', 'Gama', 'Delta'
+// Entrada: ('Gama - 1')
+// Classificação: 'Beta', 'Alfa', 'Delta', 'Gama'
+
+
